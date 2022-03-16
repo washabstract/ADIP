af04a1f61e88125587c44197c957b8062d633634
mohammedhayat <mohammed@washingtonabstract.com>
2022-03-15 21:37:06 -0700

    initial commit, introduction directory

---
+++ introduction/graphics_script.py
@@ -0,0 +1,66 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeIn(square))  # fade out animation
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(BLUE, opacity=0.5)
+        )  # color the circle on screen
+
+
+class FadeInRotate(Scene):
+  def construct(self):
+    square = Square()
+
+    self.play(FadeIn(square))
+
+    self.wait(1)
+
+    self.play(Rotate(square, PI))
+
+class TableWriteDemo(Scene):
+    def construct(self):
+        t0 = Table(
+            [["First", "Second"],
+            ["Third","Fourth"]],
+            row_labels=[Text("R1"), Text("R2")],
+            col_labels=[Text("C1"), Text("C2")],
+            top_left_entry=Text("TOP"))
+        self.play(Write(t0))
